180aee0d24805396f0a3ad4ddb7be8f9066a4060
Toji <zen_monitor@outlook.com>
2025-03-16 18:43:51 -0500

    created a base project in angular 11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-material-app';
+  showFiller = false;
+  opened = true;
+  searchQuery = '';
+
+  menuItems = [
+    { icon: 'home', label: 'Home' },
+    { icon: 'settings', label: 'Settings' },
+    { icon: 'info', label: 'About' },
+    { icon: 'mail', label: 'Messages' },
+    { icon: 'list', label: 'Tasks'},
+    { icon: 'account_circle', label: 'Profile'}
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input'; // Import MatInputModule for search bar
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +18,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatButtonModule,
+    MatInputModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
